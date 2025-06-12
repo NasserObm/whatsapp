d47ac65ddb906ae626ed11427d043bdef9635bd4
NasserObm <nassermohamedobame@gmail.com>
2025-06-12 18:11:29 +0100

    serveur config

--- src/main/java/com/groupeC/whatsapp/configurations/SecurityConfig.java
+++ src/main/java/com/groupeC/whatsapp/configurations/SecurityConfig.java
@@ -18,6 +18,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 //La classe pour les configutations du système
 @EnableMethodSecurity
@@ -46,6 +51,18 @@ public class SecurityConfig {
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->  httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
 
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Origine autorisée (Angular)
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Méthodes autorisées
+        configuration.setAllowedHeaders(List.of("*")); // Tous les headers autorisés
+        configuration.setAllowCredentials(true); // Si tu envoies des cookies ou tokens
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Appliquer à toutes les routes
+        return source;
     }
     //Gestion de la connexion
     @Bean
